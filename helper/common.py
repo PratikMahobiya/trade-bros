840474bd4aadbe6a487e0ded359aa2e0899bf062
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2024-08-20 14:34:34 +0530

    Entry Check

--- helper/common.py
+++ helper/common.py
@@ -68,8 +68,17 @@ def Check_Entry(now, configuration_obj, index_obj, days_difference):
     return True
 
   # Check Thrusday, Friday Finnifty index Target
-  elif index_obj.index in ['FINNIFTY', 'BANKNIFTY'] and days_difference in [6, 5]:
+  elif index_obj.index in ['FINNIFTY''] and days_difference in [6, 5]:
     if (sum(Transaction.objects.filter(date__date=datetime.now(tz=ZoneInfo("Asia/Kolkata")).date(), indicate='EXIT', index=index_obj.index).values_list('profit', flat=True)) > 15):
+      return True
+    elif (sum(Transaction.objects.filter(date__date=datetime.now(tz=ZoneInfo("Asia/Kolkata")).date(), indicate='EXIT', index=index_obj.index).values_list('profit', flat=True)) < -index_obj.stoploss):
+      return True
+    else:
+      return False
+
+  # Check Thrusday, Friday Banknifty index Target
+  elif index_obj.index in ['BANKNIFTY'] and days_difference in [6, 5]:
+    if (sum(Transaction.objects.filter(date__date=datetime.now(tz=ZoneInfo("Asia/Kolkata")).date(), indicate='EXIT', index=index_obj.index).values_list('profit', flat=True)) > 20):
       return True
     elif (sum(Transaction.objects.filter(date__date=datetime.now(tz=ZoneInfo("Asia/Kolkata")).date(), indicate='EXIT', index=index_obj.index).values_list('profit', flat=True)) < -index_obj.stoploss):
       return True

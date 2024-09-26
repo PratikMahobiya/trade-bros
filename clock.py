dc59f461f2f8a59ec2c184c24a573b0f1c14be30
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2024-09-26 16:30:03 +0530

    Monday to FRIDAY

--- clock.py
+++ clock.py
@@ -9,11 +9,11 @@ def start():
     socket_setup(log_identifier='Restart')
 
     # Schedules job_function to be run on the Monday to Friday
-    sched.add_job(stay_awake, 'cron',
+    sched.add_job(stay_awake, 'cron', day_of_week='mon-fri',
                 second='*/40', timezone='Asia/Kolkata')
-    sched.add_job(BrokerConnection, 'cron',
+    sched.add_job(BrokerConnection, 'cron', day_of_week='mon-fri',
                 hour='8', minute='55', timezone='Asia/Kolkata')
-    sched.add_job(socket_setup, 'cron',
-                hour='9', minute='10', timezone='Asia/Kolkata')
+    sched.add_job(socket_setup, 'cron', day_of_week='mon-fri',
+                hour='9', minute='5', timezone='Asia/Kolkata')
     sched.start()
     return True

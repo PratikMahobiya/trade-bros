1893779ac6970952b72a9c08a94dcd876d55e821
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2024-10-03 07:04:53 +0000

    Post Axit Check Added

--- helper/common.py
+++ helper/common.py
@@ -1,4 +1,13 @@
+from time import sleep
+from django.db import transaction
+from stock.models import Transaction
+from django.dispatch import receiver
 from django.utils.html import format_html
+from django.db.models.signals import post_save
+from system_conf.models import Configuration, Symbol
+from helper.angel_order import Create_Order, Is_Order_Completed
+
+
 
 def colour(value):
     if value == 0:
@@ -10,5 +19,39 @@ def colour(value):
 
 def calculate_volatility(dt):
     dt['Return'] = 100 * (dt['Close'].pct_change())
-    daily_volatility = dt['Return'].std()
-    return daily_volatility
+    return dt['Return'].std()
+
+
+def ExitAction(sender, instance, created):
+    try:
+        print(f"Pratik: POST EXIT: Check Exit Succeed or Not: {instance.product} : {instance.name} : {instance.symbol} : {instance.type} : {instance.price} :  {instance.order_id}")
+        configuration_obj = Configuration.objects.filter(product=instance.product)[0]
+        if configuration_obj.place_order and instance.indicate == 'EXIT' and not Is_Order_Completed(instance.order_id):
+            symbol_obj = Symbol.objects.get(product=instance.product, symbol=instance.symbol, is_active=True)
+            if instance.product == 'future':
+                order_id, order_status, price = Create_Order(configuration_obj, 'sell', 'CARRYFORWARD', symbol_obj.token, instance.symbol, symbol_obj.exchange, instance.price, instance.lot, "MARKET")
+            else:
+                if instance.mode == 'CE':
+                    order_id, order_status, price = Create_Order(configuration_obj, 'sell', 'DELIVERY', symbol_obj.token, instance.symbol, symbol_obj.exchange, instance.price, instance.lot, "MARKET")
+                else:
+                    order_id, order_status, price = Create_Order(configuration_obj, 'buy', 'INTRADAY', symbol_obj.token, instance.symbol, symbol_obj.exchange, instance.price, instance.lot, "MARKET")
+            
+            if configuration_obj.place_order and (order_id in ['', 0, '0', None]):
+                print(f"Pratik: POST EXIT: ERROR: Not Accepting Orders: {instance.symbol} : {order_id}, {order_status}")
+                sleep(1)
+                ExitAction(sender, instance, created)
+            else:
+                instance.order_id = order_id
+                instance.order_status = order_status
+                instance.save()
+    except Exception as e:
+        print(f"Pratik: POST EXIT: ERROR: {e}")
+    return True
+
+
+@receiver(post_save, sender=Transaction)
+def CheckExitSucceedOrNot(sender, instance, created, **kwargs):
+  if created:
+    sleep(1)
+    transaction.on_commit(lambda: ExitAction(sender, instance, created))
+
